5d3af47dd5e040cd4af5c57c3ec7d555d96ab129
Aaron Sulwer <asulwer@gmail.com>
2016-01-28 12:16:11 -0600

    added wcf project

---
+++ GenericBugService/ITransferService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+
+namespace GenericBugService
+{
+    [MessageContract]
+    public class RemoteFileInfo : IDisposable
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string ServerLocation;
+
+        [MessageBodyMember(Order = 1)]
+        public Stream FileByteStream;
+
+        public void Dispose()
+        {
+            if (FileByteStream != null)
+            {
+                FileByteStream.Close();
+                FileByteStream = null;
+            }
+        }
+    }
+
+    [ServiceContract]
+    public interface ITransferService
+    {
+        [OperationContract]
+        [FaultContract(typeof(InvalidOperationException))]
+        [FaultContract(typeof(Exception))]
+        void UploadFile(RemoteFileInfo rfi);
+    }    
+}
---
+++ GenericBugService/TransferService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace GenericBugService
+{
+    public class TransferService : ITransferService
+    {
+        public void UploadFile(RemoteFileInfo rfi)
+        {
+            FileStream targetStream = null;
+
+            using (Stream sourceStream = rfi.FileByteStream)
+            {
+                using (targetStream = new FileStream(rfi.ServerLocation, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    const int bufferLen = 65000;
+                    byte[] buffer = new byte[bufferLen];
+                    int count = 0;
+
+                    while ((count = sourceStream.Read(buffer, 0, bufferLen)) > 0)
+                    {
+                        targetStream.Write(buffer, 0, count);
+                    }
+                }
+            }
+        }
+    }
+}
